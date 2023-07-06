6ce6a59d7c6987d8cfb7f001c962415899f76149
Kayanski <kowalski.kowalskin@gmail.com>
2023-07-06 11:46:53 +0200

    Added osmo-test-5 assets

--- src/chains/Osmosis.ts
+++ src/chains/Osmosis.ts
@@ -3,11 +3,12 @@ import { Chain } from './chain'
 import { PoolRegistry } from '../registry/poolRegistry'
 import { AssetRegistry } from '../registry/assetRegistry'
 import { Osmosis1 } from '../networks/osmosis1'
+import { OsmoTest5 } from '../networks/osmotest5'
 
 const osmosis1 = new Osmosis1(new AssetRegistry(), new ContractRegistry(), new PoolRegistry())
-
+const osmotest5 = new OsmoTest5(new AssetRegistry(), new ContractRegistry(), new PoolRegistry())
 export class Osmosis extends Chain {
   constructor() {
-    super('osmosis', [osmosis1])
+    super('osmosis', [osmosis1, osmotest5])
   }
 }
