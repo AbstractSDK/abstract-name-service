f8b1848ee7933acc0aa08bc6bb29630cc8ee1728
adairrr <32375605+adairrr@users.noreply.github.com>
2024-01-31 09:31:29 -0800

    Update ANS for neutron

--- cron/src/bin/update_ans.rs
+++ cron/src/bin/update_ans.rs
@@ -6,15 +6,29 @@ use cw_orch::{
         networks::{ChainInfo, parse_network},
     },
 };
+use cw_orch::daemon::ChainKind;
+use cw_orch::daemon::networks::neutron::NEUTRON_NETWORK;
 use tokio::runtime::Runtime;
+
+/// <https://github.com/cosmos/chain-registry/blob/master/neutron/chain.json>
+pub const NEUTRON_1: ChainInfo = ChainInfo {
+    kind: ChainKind::Mainnet,
+    chain_id: "neutron-1",
+    gas_denom: "untrn",
+    gas_price: 0.25,
+    grpc_urls: &["http://grpc-kralum.neutron-1.neutron.org:80"],
+    network_info: NEUTRON_NETWORK,
+    lcd_url: Some("https://rest-kralum.neutron-1.neutron.org"),
+    fcd_url: None,
+};
 
 fn update_ans() -> anyhow::Result<()> {
     let rt = Runtime::new()?;
     let deployment = Abstract::load_from(Mock::new(&Addr::unchecked("input")))?;
     // let chain_ids = deployment.get_all_deployed_chains();
-    let chain_ids: Vec<String> = vec!["juno-1"].into_iter().map(|n| n.to_string()).collect();
+    let chain_ids: Vec<String> = vec!["neutron-1"].into_iter().map(|n| n.to_string()).collect();
 
-    let networks: Vec<ChainInfo> = chain_ids.iter().map(|n| parse_network(n)).collect();
+    let networks: Vec<ChainInfo> = vec![NEUTRON_1];
 
     for network in networks {
         let chain = DaemonBuilder::default()
